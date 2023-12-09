cb2f824d59c08f52ec2356a62a0905a5178c4c6b
Nahtan <nathan.titirangi@gmail.com>
2023-12-09 15:47:12 +1300

    cleaner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import copy
-frontier = [(0,[])]
+frontier = [(0, [])]
 
 mods = {"chain spell": -30,
         "reduce lifetime": -42,
@@ -10,46 +10,46 @@ mods = {"chain spell": -30,
         "null shot": 280
         }
 
-inv = {v: k for k,v in mods.items()}
+inv = {v: k for k, v in mods.items()}
 
-all = [x for k,x in mods.items()]
-negative = [x for k,x in mods.items() if x < 0]
-positive = [x for k,x in mods.items() if x > 0]
+all = [x for k, x in mods.items()]
+negative = [x for k, x in mods.items() if x < 0]
+positive = [x for k, x in mods.items() if x > 0]
 
 target = int(input("target: "))
 count = input("find: ")
 if count == "":
-	count = "100"
+    count = "100"
 count = int(count)
 sols = []
 searched = set()
 done = 0
 while True:
-	e = frontier[0]
-	frontier.pop(0)
-	e[1].sort()
-	key = tuple(e[1].count(x) for x in all)
-	# print(e[1],key)
-	if key in searched:
-		continue
-	searched.add(key)
-	if e[0] > target:
-		for elem in negative:
-			n = copy.copy(e[1])
-			n.append(elem)
-			frontier.append((e[0]+elem,n))
-	elif e[0] < target:
-		for elem in positive:
-			n = copy.copy(e[1])
-			n.append(elem)
-			frontier.append((e[0]+elem,n))
-	else:
-		e[1].sort()
-		done += 1
-		sols.append(key)
-		if done >= count:
-			break
+    e = frontier[0]
+    frontier.pop(0)
+    e[1].sort()
+    key = tuple(e[1].count(x) for x in all)
+    if key in searched:
+        continue
+    searched.add(key)
+    if e[0] > target:
+        for elem in negative:
+            n = copy.copy(e[1])
+            n.append(elem)
+            frontier.append((e[0]+elem, n))
+    elif e[0] < target:
+        for elem in positive:
+            n = copy.copy(e[1])
+            n.append(elem)
+            frontier.append((e[0]+elem, n))
+    else:
+        e[1].sort()
+        done += 1
+        sols.append(key)
+        if done >= count:
+            break
 
 sols.reverse()
-sols = [", ".join([f"{x} * {inv[all[k]]}" for k,x in enumerate(sol) if x > 0]) for sol in sols]
+sols = [", ".join([f"{x} * {inv[all[k]]}" for k,
+                  x in enumerate(sol) if x > 0]) for sol in sols]
 print("\n".join(sols))
