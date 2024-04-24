554b3a23a671d8aa76d80fbf6923c463a4ad1644
NathanSnail <nathan.titirangi@gmail.com>
2024-04-24 15:11:33 +1200

    add ranges (low testing so might have bugs, should still be optimal always)

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ mods = {
 def div_eval(vals):
 	t = {0: 0, 1: 1, 2: 2, 3: 2, 4: 2, 5: 2, 6: 3, 7: 3, 8: 3, 9: 3, 10: 3, 11: 2}
 	score = sum([t[x] if x in t.keys() else math.ceil(x / 3) for x in vals])
-	print(vals, score)
 	return score
 
 
@@ -27,7 +26,17 @@ all = [x for k, x in mods.items()]
 negative = [x for k, x in mods.items() if x < 0]
 positive = [x for k, x in mods.items() if x > 0]
 
-target = int(input("target: "))
+target = input("target: ")
+if target.find("."):
+	bounds = [int(x) for x in target.split(".")]
+	upper = max(bounds)
+	lower = min(bounds)
+else:
+	itarget = int(target)
+	upper = itarget
+	lower = itarget
+target_ranged = upper != lower
+
 count = input("find: ")
 if count == "":
 	count = "100"
@@ -35,26 +44,37 @@ count = int(count)
 sols = []
 searched = set()
 done = 0
+
+
+def new_case(value, stack, elem):
+	n = [x for x in stack] + [elem]
+	frontier.append((value + elem, n))
+
+
 while True:
 	e = frontier[0]
+	value = e[0]
+	stack = e[1]
+	stack.sort()
 	frontier.pop(0)
-	e[1].sort()
 	key = tuple(e[1].count(x) for x in all)
 	if key in searched:
 		continue
 	searched.add(key)
-	if e[0] > target:
+	if value > upper:
 		for elem in negative:
-			n = copy.copy(e[1])
-			n.append(elem)
-			frontier.append((e[0] + elem, n))
-	elif e[0] < target:
+			new_case(value, stack, elem)
+	elif value < lower:
 		for elem in positive:
-			n = copy.copy(e[1])
-			n.append(elem)
-			frontier.append((e[0] + elem, n))
+			new_case(value, stack, elem)
 	else:
-		e[1].sort()
+		if target_ranged:
+			if upper != e[0]:
+				for elem in positive:
+					new_case(value, stack, elem)
+			if lower != e[0]:
+				for elem in negative:
+					new_case(value, stack, elem)
 		done += 1
 		sols.append(key)
 		if done >= count:
@@ -63,7 +83,7 @@ while True:
 sols.sort(key=lambda x: -div_eval(x))
 sols = [
 	", ".join([f"{x} * {inv[all[k]]}" for k, x in enumerate(sol) if x > 0])
-	+ f" ({div_eval(sol)})"
+	+ f" (estimated {div_eval(sol)} spells)"
 	for sol in sols
 ]
 print("\n".join(sols))
